7c1ad64ab74096f635fc362cfd939f1b76c2d957
FilipSzweda <filip.szweda1@gmail.com>
2023-05-03 00:25:29 +0200

    LNX-124 Adapt to atomic `Push`

--- entity/Entity.gd
+++ entity/Entity.gd
@@ -14,20 +14,6 @@ func _populate(attributes_json: Dictionary):
 		if attributes_json.has(accepted_attribute):
 			var attribute = "_" + accepted_attribute
 			var attribute_value = self.get(attribute)
-			if attribute_value is Array:
-				var internal_type = attribute_value.get_typed_class_name()
-				if internal_type is int or internal_type is float or internal_type is bool or internal_type is String:
-					for element in attributes_json.get(accepted_attribute):
-						attribute_value.append(element)
-				elif internal_type is Vector2:
-					for element in attributes_json.get(accepted_attribute):
-						var vector2 = Vector2(element.get("x"), element.get("y"))
-						attribute_value.append(vector2)
-				elif internal_type is LynxEntity:
-					for element in attributes_json.get(accepted_attribute):
-						attribute_value.append(entity_deserializer.deserialize(element))
-				else:
-					print("[ERROR] Unknown Array attribute type when populating")
 			if attribute_value is int or attribute_value is float or attribute_value is bool or attribute_value is String:
 				self.set(attribute, attributes_json.get(accepted_attribute))
 			elif attribute_value is Vector2:
@@ -35,6 +21,25 @@ func _populate(attributes_json: Dictionary):
 				self.set(attribute, vector2)
 			elif attribute_value is LynxEntity:
 				self.set(attribute, entity_deserializer.deserialize(attributes_json.get(accepted_attribute)))
+			elif attribute_value is Array[int]:
+				for element in attributes_json.get(accepted_attribute):
+					attribute_value.append(int(element))
+			elif attribute_value is Array[float]:
+				for element in attributes_json.get(accepted_attribute):
+					attribute_value.append(float(element))
+			elif attribute_value is Array[bool]:
+				for element in attributes_json.get(accepted_attribute):
+					attribute_value.append(bool(element))
+			elif attribute_value is Array[String]:
+				for element in attributes_json.get(accepted_attribute):
+					attribute_value.append(String(element))
+			elif attribute_value is Array[Vector2]:
+				for element in attributes_json.get(accepted_attribute):
+					var vector2 = Vector2(element.get("x"), element.get("y"))
+					attribute_value.append(vector2)
+			elif attribute_value is Array[LynxEntity]:
+				for element in attributes_json.get(accepted_attribute):
+					attribute_value.append(entity_deserializer.deserialize(element))
 			else:
 				print("[ERROR] Unknown attribute type when populating")
 
