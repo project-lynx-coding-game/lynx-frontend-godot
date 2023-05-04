1c4f7ab8e16a90a0ab5238eeec4d005923b9eac0
Mateusz Grzonka <alpinus4@gmail.com>
2023-05-04 19:03:02 +0200

    Add separate function for tile deserialization, fix error message
    Decoupling entity deserialization and tile deserialization logic would
    be very inefficient.
    Removed the unnecesary error, now we don't get pointless error about
    failing to deserialize entity.
    Errors with deserializing are handled by EntityDeserializer.
    
    Moved tile logic to separate function.

--- scene/world_updater/SceneRecreator.gd
+++ scene/world_updater/SceneRecreator.gd
@@ -8,14 +8,13 @@ extends Node
 func recreate_scene(scene_json):
 	for entity_json in scene_json["entities"]:
 		var entity = entity_deserializer.deserialize(entity_json)
-		
 		if !entity:
-			print("[ERROR] Could not deserialize Entity")
 			continue
 		
 		# TODO: this could be done at beginning of the loop by checking type key value
 		if !entity is LynxObject:
 			print("[ERROR] Entity could not be recreated, because it is not an Object")
+			entity.queue_free()
 			continue
 		
 		objects_container.add_child(entity)
