9fd7bf6b2230b60d9a30fab7726194ac42975666
Krystian Jandy <krystian.jandy0130@gmail.com>
2023-05-07 19:40:51 +0200

    LNX-125 Add free moving camera

---
+++ Camera2D.gd
@@ -0,0 +1,45 @@
+extends Camera2D
+
+@export var move_speed = 12.0
+@export var border_margin = 1
+@export var smoothness = 10
+@export var zoom_speed = 0.1
+@export var max_zoom = Vector2(10.0, 10.0)
+@export var min_zoom = Vector2(1.0, 1.0)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			zoom = zoom / pow(1 + zoom_speed, 1)
+			zoom = clamp(zoom, min_zoom, max_zoom)
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			zoom = zoom * pow(1 + zoom_speed, 1)
+			zoom = clamp(zoom, min_zoom, max_zoom)
+
+func _process(delta):
+	var viewport_rect = get_viewport_rect()
+	var camera_pos  = position
+	var moved = false
+	
+	# Check if the mouse is near the left edge of the screen 
+	if get_viewport().get_mouse_position().x < viewport_rect.position.x + border_margin:
+		camera_pos.x = lerp(camera_pos.x, camera_pos.x - move_speed, smoothness * delta)
+		moved = true
+	
+	# Check if the mouse is near the right edge of the screen
+	if get_viewport().get_mouse_position().x > viewport_rect.position.x + viewport_rect.size.x - border_margin:
+		camera_pos.x = lerp(camera_pos.x, camera_pos.x + move_speed, smoothness * delta)
+		moved = true
+	
+	# Check if the mouse is near the top edge of the screen
+	if get_viewport().get_mouse_position().y < viewport_rect.position.y + border_margin:
+		camera_pos.y = lerp(camera_pos.y, camera_pos.y - move_speed, smoothness * delta)
+		moved = true
+	
+	# Check if the mouse is near the bottom edge of the screen
+	if get_viewport().get_mouse_position().y > viewport_rect.position.y + viewport_rect.size.y - border_margin:
+		camera_pos.y = lerp(camera_pos.y, camera_pos.y + move_speed, smoothness * delta)
+		moved = true
+	
+	if moved:
+		position = camera_pos
