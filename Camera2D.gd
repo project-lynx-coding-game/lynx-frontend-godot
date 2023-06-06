336f9031a020bb8651c2f5410b2c0cbe3d7b5cfd
Krystian Jandy <krystian.jandy0130@gmail.com>
2023-06-06 22:38:58 +0200

    fix comments from pr

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,18 +1,18 @@
 extends Camera2D
 
 @export var move_speed = 12.0
-@export var border_margin = 200
+@export var border_margin = 150
 @export var smoothness = 10
 @export var zoom_speed = 0.1
 @export var max_zoom = Vector2(10.0, 10.0)
 @export var min_zoom = Vector2(1.0, 1.0)
-var mouse_exit = false
+var is_mouse_outside_screen = false
 
 func _notification(what):
 	if what == NOTIFICATION_WM_MOUSE_EXIT:
-		mouse_exit = true
+		is_mouse_outside_screen = true
 	if what == NOTIFICATION_WM_MOUSE_ENTER:
-		mouse_exit = false
+		is_mouse_outside_screen = false
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -26,33 +26,34 @@ func _input(event):
 			self._reset_camera_position(get_viewport_rect())
 
 func _process(delta):
-	if mouse_exit:
+	if is_mouse_outside_screen:
 		return
 	
 	var viewport_rect = get_viewport_rect()
 	var camera_pos  = position
 		
 	var can_move = false
-	var x_mouse_position = get_viewport().get_mouse_position().x
-	var y_mouse_position = get_viewport().get_mouse_position().y
+	var mouse_x = get_viewport().get_mouse_position().x
+	var mouse_y = get_viewport().get_mouse_position().y
 	
-	# LEFT EDGE
-	if x_mouse_position < viewport_rect.position.x + border_margin:
+	var left_screen_edge = viewport_rect.position.x + border_margin
+	var right_screen_edge = viewport_rect.position.x + viewport_rect.size.x - border_margin
+	var top_screen_edge = viewport_rect.position.y + border_margin
+	var bottom_screen_edge = viewport_rect.position.y + viewport_rect.size.y - border_margin
+	
+	if mouse_x < left_screen_edge:
 		camera_pos.x = lerp(camera_pos.x, camera_pos.x - move_speed, smoothness * delta)
 		can_move = true
 	
-	# RIGHT EDGE
-	if x_mouse_position > viewport_rect.position.x + viewport_rect.size.x - border_margin:
+	if mouse_x > right_screen_edge:
 		camera_pos.x = lerp(camera_pos.x, camera_pos.x + move_speed, smoothness * delta)
 		can_move = true
 	
-	# TOP EDGE
-	if y_mouse_position < viewport_rect.position.y + border_margin:
+	if mouse_y < top_screen_edge:
 		camera_pos.y = lerp(camera_pos.y, camera_pos.y - move_speed, smoothness * delta)
 		can_move = true
 	
-	# BOTTOM EDGE
-	if y_mouse_position > viewport_rect.position.y + viewport_rect.size.y - border_margin:
+	if mouse_y > bottom_screen_edge:
 		camera_pos.y = lerp(camera_pos.y, camera_pos.y + move_speed, smoothness * delta)
 		can_move = true
 	
