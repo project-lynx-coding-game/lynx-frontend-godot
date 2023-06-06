2a134f59ab2f7e5c12e33cc35869d01e3a63c4d0
Krystian Jandy <101421631+Krystian030@users.noreply.github.com>
2023-06-06 23:18:59 +0200

    LNX-151 Improve camera movement (#16)
    * LNX-151 Improve camera movement
    
    * fix comments from pr
    
    ---------
    
    Co-authored-by: Krystian Jandy <krystian.jandy0130@gmail.com>

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,13 +1,20 @@
 extends Camera2D
 
 @export var move_speed = 12.0
-@export var border_margin = 10
+@export var border_margin = 150
 @export var smoothness = 10
 @export var zoom_speed = 0.1
 @export var max_zoom = Vector2(10.0, 10.0)
 @export var min_zoom = Vector2(1.0, 1.0)
+var is_mouse_outside_screen = false
 
-func _unhandled_input(event):
+func _notification(what):
+	if what == NOTIFICATION_WM_MOUSE_EXIT:
+		is_mouse_outside_screen = true
+	if what == NOTIFICATION_WM_MOUSE_ENTER:
+		is_mouse_outside_screen = false
+
+func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			zoom = zoom / pow(1 + zoom_speed, 1)
@@ -15,35 +22,43 @@ func _unhandled_input(event):
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			zoom = zoom * pow(1 + zoom_speed, 1)
 			zoom = clamp(zoom, min_zoom, max_zoom)
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			self._reset_camera_position(get_viewport_rect())
 
 func _process(delta):
-	# TODO: Add restrictions to disable camera movement when the player moves the mouse away from the map
+	if is_mouse_outside_screen:
+		return
+	
 	var viewport_rect = get_viewport_rect()
 	var camera_pos  = position
-	var moved = false
+		
+	var can_move = false
+	var mouse_x = get_viewport().get_mouse_position().x
+	var mouse_y = get_viewport().get_mouse_position().y
 	
-	# LEFT EDGE
-	if get_viewport().get_mouse_position().x < viewport_rect.position.x + border_margin:
+	var left_screen_edge = viewport_rect.position.x + border_margin
+	var right_screen_edge = viewport_rect.position.x + viewport_rect.size.x - border_margin
+	var top_screen_edge = viewport_rect.position.y + border_margin
+	var bottom_screen_edge = viewport_rect.position.y + viewport_rect.size.y - border_margin
+	
+	if mouse_x < left_screen_edge:
 		camera_pos.x = lerp(camera_pos.x, camera_pos.x - move_speed, smoothness * delta)
-		moved = true
+		can_move = true
 	
-	# RIGHT EDGE
-	if get_viewport().get_mouse_position().x > viewport_rect.position.x + viewport_rect.size.x - border_margin:
+	if mouse_x > right_screen_edge:
 		camera_pos.x = lerp(camera_pos.x, camera_pos.x + move_speed, smoothness * delta)
-		moved = true
+		can_move = true
 	
-	# TOP EDGE
-	if get_viewport().get_mouse_position().y < viewport_rect.position.y + border_margin:
+	if mouse_y < top_screen_edge:
 		camera_pos.y = lerp(camera_pos.y, camera_pos.y - move_speed, smoothness * delta)
-		moved = true
+		can_move = true
 	
-	# BOTTOM EDGE
-	if get_viewport().get_mouse_position().y > viewport_rect.position.y + viewport_rect.size.y - border_margin:
+	if mouse_y > bottom_screen_edge:
 		camera_pos.y = lerp(camera_pos.y, camera_pos.y + move_speed, smoothness * delta)
-		moved = true
+		can_move = true
 	
-	if moved:
+	if can_move:
 		position = camera_pos
+
+func _reset_camera_position(viewport_rect):
+	position = Vector2(0,  0)
