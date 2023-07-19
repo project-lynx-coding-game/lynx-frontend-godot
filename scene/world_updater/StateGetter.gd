a90d3064040e3fb8d2a01f100fac435cb59bf980
EasyJBL <kubaprofesor@gmail.com>
2023-07-19 02:06:58 +0200

    lnx-164 Delay based on previous delays, so we can send get request a little bit before this time

--- scene/world_updater/StateGetter.gd
+++ scene/world_updater/StateGetter.gd
@@ -7,8 +7,24 @@ extends Node
 
 var json = JSON.new()
 var current_tick_number: int = -1
-var get_state_wait_time: float = 1.0
-var acceptable_delay: float = 0.01
+const ACCEPTABLE_DELAY : float = 0.01
+var delays: Array = []
+var last_delay: float = 0.0
+
+func _average_time():
+	var sum := 0.0
+	if delays.size() == 0:
+		return 0.0
+	for n in delays:
+		sum += n
+	return sum / delays.size()
+
+func _normalize_second(lower_time, higher_time):
+	var difference_in_ms = float(int(higher_time - lower_time) % 1000) / 1000
+	var difference_in_ms_normalized = difference_in_ms
+	if difference_in_ms_normalized >= 0.85:
+		difference_in_ms_normalized =  difference_in_ms -  Globals.DEFAULT_GET_STATE_WAIT_TIME
+	return difference_in_ms_normalized
 
 func _speed_up_actions():
 	for object in Globals.WORLD_UPDATER.objects_container.get_children():
@@ -28,9 +44,14 @@ func _update_state(response):
 		scene_wiper.wipe()
 		scene_recreator.recreate_scene(json.get_data())
 	elif "deltas" in response.keys():
+		if delays.size() == 10:
+			delays.pop_front()
+		var difference_in_ms_normalized = _normalize_second(response["send_time_ms"], Time.get_unix_time_from_system() * 1000)
+		delays.push_back(difference_in_ms_normalized + last_delay)
+		last_delay = _average_time()
 		deltas_applier.apply_deltas(response["deltas"])
-	
 	_handle_lag(current_tick_number, response["tick_number"])
+	
 	current_tick_number = response["tick_number"]
 
 func _on_get_state_http_request_request_completed(_result, response_code, _headers, body):
@@ -39,13 +60,7 @@ func _on_get_state_http_request_request_completed(_result, response_code, _heade
 		var response = json.get_data()
 		_update_state(response)
 		
-		if current_tick_number % 3 == 0:
-			var difference_in_time = Time.get_unix_time_from_system()*1000
-			var delay_time = 0 if (float(int(difference_in_time) % 1000)) / 1000 < self.acceptable_delay else (float(int(difference_in_time) % 1000)) / 1000
-			var wait_time_delay = delay_time if delay_time < 0.85 else -(1 - delay_time)
-			get_node("GetStateTimer").wait_time = 1 - wait_time_delay
-		else:
-			get_node("GetStateTimer").wait_time = 1
+		
 
 
 # send get state requests every timer timeout (1s)
@@ -54,4 +69,9 @@ func _on_get_state_timer_timeout():
 		var result = get_state_http_request.request(Globals.SERVER_ADDRESS + "?tick_number=" + str(current_tick_number))
 		if result != OK:
 			push_error("[ERROR] Could not GET state: " + str(result))
-	
+		
+		var delay_time_normalized = _normalize_second(0, Time.get_unix_time_from_system() * 1000)
+		if current_tick_number % 3 == 0:
+			get_node("GetStateTimer").wait_time = Globals.DEFAULT_GET_STATE_WAIT_TIME - delay_time_normalized - _average_time()
+		else:
+			get_node("GetStateTimer").wait_time = Globals.DEFAULT_GET_STATE_WAIT_TIME 
