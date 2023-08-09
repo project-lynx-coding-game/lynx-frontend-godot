6d13aca7186666504fe451b665d06f1aca01bce6
Krystian Jandy <krystianj@meritus.pl>
2023-08-09 18:57:27 +0200

    LNX-178 Add print builtin

---
+++ entity/action/print/Print.gd
@@ -0,0 +1,10 @@
+extends LynxAction
+
+var _text: String
+@onready var _output_console = get_node("/root/Scene/CanvasLayer/UI/AgentCreator/ColorRect/OutputConsole")
+
+func _init():
+	self.accepted_attributes = ["text"]
+
+func _execute():
+	self._output_console.print(self._text, Color.WHITE)
