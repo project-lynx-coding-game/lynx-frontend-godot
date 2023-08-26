413abad8c85871a8bf7035f65528c0062dd55116
Krystian Jandy <krystianj@meritus.pl>
2023-08-27 00:18:19 +0200

    LNX-181 Distinguish agents

--- entity/object/agent/Agent.gd
+++ entity/object/agent/Agent.gd
@@ -2,12 +2,18 @@ extends LynxObject
 class_name LynxAgent
 
 var _tick = String()
+@onready var _border = get_node("Border")
+
+func _ready():
+	if Globals.USER_ID == self._owner:
+		self._border.set_default_color(Color.DARK_BLUE)
 
 func init(_position, _id, _owner, _tick):
 	self._position = _position
 	self._id = _id
 	self._owner = _owner
 	self._tick = _tick
+
 
 # TODO: move to Entity
 func serialize():
