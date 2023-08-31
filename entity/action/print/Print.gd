2e0b0d197e9e79cc7df3d0c26d4924f261249baf
Krystian Jandy <krystianj@meritus.pl>
2023-08-31 22:55:23 +0200

    chore: change print audio

--- entity/action/print/Print.gd
+++ entity/action/print/Print.gd
@@ -4,7 +4,7 @@ var _text: String
 var _user_id: String
 
 @onready var _output_console = get_node("/root/Scene/CanvasLayer/UI/AgentCreator/ColorRect/OutputConsole")
-@onready var _audio_effect = load("res://assets/Music/error_message.wav")
+@onready var _audio_effect = load("res://assets/Music/message.wav")
 
 func _init():
 	self.accepted_attributes = ["text", "user_id"]
