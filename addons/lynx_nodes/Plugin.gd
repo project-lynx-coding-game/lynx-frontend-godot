6cce5164ffbe4c9eaf4cc1b99d45fcf78a1e7ae5
FilipSzweda <filip.szweda1@gmail.com>
2023-09-12 23:17:06 +0200

    LNX-174 Remove old sprite set

--- addons/lynx_nodes/Plugin.gd
+++ addons/lynx_nodes/Plugin.gd
@@ -2,9 +2,10 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("LynxObjectNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxObjectNode.gd"), preload("res://assets/NinjaAdventure/Items/Potion/LifePot.png"))
-	add_custom_type("LynxPlayerObjectNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxPlayerObjectNode.gd"), preload("res://assets/NinjaAdventure/Items/Potion/WaterPot.png"))
-	add_custom_type("LynxAgentNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxAgentNode.gd"), preload("res://assets/NinjaAdventure/Actor/Characters/Villager/SeparateAnim/Special1.png"))
+	var lynx_logo = preload("res://assets/Lynx/lynx-logo.jpg")
+	add_custom_type("LynxObjectNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxObjectNode.gd"), lynx_logo)
+	add_custom_type("LynxPlayerObjectNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxPlayerObjectNode.gd"), lynx_logo)
+	add_custom_type("LynxAgentNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxAgentNode.gd"), lynx_logo)
 
 func _exit_tree():
 	remove_custom_type("LynxObjectNode")
