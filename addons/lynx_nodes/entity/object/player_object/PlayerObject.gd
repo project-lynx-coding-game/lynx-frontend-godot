2bb05b7391e8627f5fb49a5365d54532075d3ae9
FilipSzweda <filip.szweda1@gmail.com>
2023-09-24 13:45:42 +0200

    NOISSUE Fix color_with_outline.gdshader

--- addons/lynx_nodes/entity/object/player_object/PlayerObject.gd
+++ addons/lynx_nodes/entity/object/player_object/PlayerObject.gd
@@ -2,13 +2,12 @@ extends LynxObject
 class_name LynxPlayerObject
 
 func _generate_color_from_id(agent_id: int) -> Color:
-	# Hash agent_id for more diverse color generation.
 	var hashed_id = hash(agent_id) 
 	var r = ((hashed_id >> 5) & 0xFF) / 255.0
 	var g = ((hashed_id >> 13) & 0xFF) / 255.0
 	var b = ((hashed_id >> 21) & 0xFF) / 255.0
 	
-	return Color(r, g, b)
+	return Color(r, g, b, 1.0)
 
 func _ready():
 	var color = self._generate_color_from_id(int(self._owner))
