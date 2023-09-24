203167108de7b33f10e74d9eecdc4b906d60c7c1
Filip Szweda <68189467+filip-szweda@users.noreply.github.com>
2023-09-24 14:37:49 +0200

    NOISSUE Fix color_with_outline.gdshader (#52)

--- addons/lynx_nodes/Plugin.gd
+++ addons/lynx_nodes/Plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	var lynx_logo = preload("res://assets/Lynx/lynx-logo.jpg")
+	var lynx_logo = preload("res://assets/Lynx/lynx-logo-small.png")
 	add_custom_type("LynxObjectNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxObjectNode.gd"), lynx_logo)
 	add_custom_type("LynxPlayerObjectNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxPlayerObjectNode.gd"), lynx_logo)
 	add_custom_type("LynxAgentNode", "Node2D", preload("res://addons/lynx_nodes/nodes/LynxAgentNode.gd"), lynx_logo)
