89942f2cd63db2efb66092af2e197e672d648920
EasyJBL <kubaprofesor@gmail.com>
2023-10-01 03:25:01 +0200

    LNX-140 add button press

--- ui/Settings.gd
+++ ui/Settings.gd
@@ -8,5 +8,6 @@ func _on_settings_button_toggled(button_pressed):
 	get_node("SettingsPanel/VBoxContainer/AddressTextEdit").text = Globals.SERVER_ADDRESS
 
 func _on_save_button_button_up():
+	get_node("../ButtonPress").play()
 	Globals.SERVER_ADDRESS = get_node("SettingsPanel/VBoxContainer/AddressTextEdit").text
 	get_node("SettingsButton").button_pressed = false
