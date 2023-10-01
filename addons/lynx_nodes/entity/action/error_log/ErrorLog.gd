b678e9c41f713f7918e71b54c0a66de7635880c1
EasyJBL <kubaprofesor@gmail.com>
2023-10-01 03:25:01 +0200

    LNX-140 Add audio effects

--- addons/lynx_nodes/entity/action/error_log/ErrorLog.gd
+++ addons/lynx_nodes/entity/action/error_log/ErrorLog.gd
@@ -4,6 +4,7 @@ var _text: String
 var _user_id: String
 
 @onready var _output_console = get_node("/root/Scene/CanvasLayer/UI/AgentCreator/ColorRect/OutputConsole")
+@onready var _audio_sound = load("res://assets/Music/error_message.wav")
 
 func _init():
 	self.accepted_attributes = ["text", "user_id"]
@@ -11,3 +12,4 @@ func _init():
 func _execute():
 	if _user_id == Globals.USER_ID:
 		self._output_console.print(self._text, Color.DARK_RED)
+		self.play_global_action(self._audio_sound)
--- addons/lynx_nodes/nodes/LynxAgentNode.gd
+++ addons/lynx_nodes/nodes/LynxAgentNode.gd
@@ -5,6 +5,7 @@ var action_queue: Node
 var execute_action_timer: Timer
 var speech_bubble: Control
 var border: Line2D
+var audio_stream_player_2d: AudioStreamPlayer2D
 
 @export var action_speed: float
 
@@ -12,3 +13,4 @@ func _enter_tree():
 	_setup_action_queue()
 	_setup_execute_action_timer()
 	_setup_speech_bubble()
+	_setup_audio_stream_player_2d()
