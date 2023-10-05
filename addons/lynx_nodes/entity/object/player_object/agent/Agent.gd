fa693269cae61f7c3beb082d6cb5e961b9e5bbfe
EasyJBL <kubaprofesor@gmail.com>
2023-10-06 00:53:37 +0200

    lnx-140 comments lynx node

--- addons/lynx_nodes/entity/object/player_object/agent/Agent.gd
+++ addons/lynx_nodes/entity/object/player_object/agent/Agent.gd
@@ -4,8 +4,8 @@ class_name LynxAgent
 var _tick = String()
 
 @onready var active_item = get_node("ActiveItem")
-@onready var _audio_node = get_node("SoundEffect")
 @onready var camera = get_node("/root/Scene/Camera2D")
+
 func init(_position, _id, _owner, _tick):
 	self._position = _position
 	self._id = _id
@@ -58,12 +58,13 @@ func end_active_item_animation(animation_name: String, direction: Vector2):
 #Function used to play sound around agents, like sound animation of action
 func play_audio(effect: Object, obj_position: Vector2i):
 	var screen_size = DisplayServer.window_get_size() / camera.zoom.x /2
-	if obj_position.x + 8 < camera.position.x + screen_size.x and \
-		obj_position.x + 8 > camera.position.x - screen_size.x and \
-		obj_position.y + 8 < camera.position.y + screen_size.y and \
-		obj_position.y + 8 > camera.position.y - screen_size.y:
-		print(typeof(effect))
-		self._audio_node.stream = effect
-		self._audio_node.max_distance = screen_size.x
-		self._audio_node.volume_db = - (11 - camera.zoom.x) * 2
-		self._audio_node.play()
+	var square_offset = 8 # Size of square = 16
+	var zoom_normalization = 11
+	if obj_position.x + square_offset < camera.position.x + screen_size.x and \
+		obj_position.x + square_offset > camera.position.x - screen_size.x and \
+		obj_position.y + square_offset < camera.position.y + screen_size.y and \
+		obj_position.y + square_offset > camera.position.y - screen_size.y:
+		self.audio_stream_player_2d.stream = effect
+		self.audio_stream_player_2d.max_distance = screen_size.x			#Experiment with this, maybe make better calc, than that
+		self.audio_stream_player_2d.volume_db = - (zoom_normalization - camera.zoom.x) * 2
+		self.audio_stream_player_2d.play()
